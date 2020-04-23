1e549ce501192e4aa2ade8c9fa50bf37c921646d
chentsulin <chentsulin@gmail.com>
2020-04-23 14:45:20 +0800

    first commit

---
+++ bottender.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  session: {
+    driver: 'memory',
+    stores: {
+      memory: {
+        maxSize: 500,
+      },
+      file: {
+        dirname: '.sessions',
+      },
+      redis: {
+        port: 6379,
+        host: '127.0.0.1',
+        password: 'auth',
+        db: 0,
+      },
+      mongo: {
+        url: 'mongodb://localhost:27017',
+        collectionName: 'sessions',
+      },
+    },
+  },
+  initialState: {},
+  channels: {
+    messenger: {
+      enabled: true,
+      path: '/webhooks/messenger',
+      pageId: process.env.MESSENGER_PAGE_ID,
+      accessToken: process.env.MESSENGER_ACCESS_TOKEN,
+      appId: process.env.MESSENGER_APP_ID,
+      appSecret: process.env.MESSENGER_APP_SECRET,
+      verifyToken: process.env.MESSENGER_VERIFY_TOKEN,
+    },
+  },
+};
---
+++ src/index.js
@@ -0,0 +1,59 @@
+const { getClient } = require('bottender');
+const { router, text, messenger } = require('bottender/router');
+
+// 為了 demo 效果讓它保持 false
+const inStock = false;
+const client = getClient('messenger');
+
+async function HandleQuestion(context) {
+  if (!inStock) {
+    await context.sendText(
+      '很不幸，現在 Switch 沒有庫存，但我們可以在補貨時立刻通知你！'
+    );
+    await context.sendOneTimeNotifReqTemplate({
+      title: '當 Switch 補貨的時候',
+      payload: 'NOTIFY_ME_WHEN_IN_STOCK',
+    });
+  }
+
+  await context.sendText('很幸運，現在 Switch 還有庫存！');
+}
+
+async function HandleOptin(context) {
+  const { optin } = context.event;
+  // optin.type -> 會是 "one_time_notif_req"
+  // optin.payload -> 前面 `context.sendOneTimeNotifReqTemplate` 所送出的 payload
+  if (
+    optin.type === 'one_time_notif_req' &&
+    optin.payload === 'NOTIFY_ME_WHEN_IN_STOCK'
+  ) {
+    // 收到 token 啦：optin.oneTimeNotifToken
+    // 為了 demo 效果在一分鐘後直接通知到貨
+    setTimeout(() => {
+      client.sendGenericTemplate(
+        { oneTimeNotifToken: optin.oneTimeNotifToken },
+        [
+          {
+            title: 'Switch 有現貨了!快來買！',
+            imageUrl:
+              'https://www.nintendo.tw/hardware/switch/feature/img/01-hero/local-hero__model-pic.png',
+            buttons: [
+              {
+                type: 'web_url',
+                url: 'https://www.nintendo.tw/hardware/switch/feature/',
+                title: '買起來',
+              },
+            ],
+          },
+        ]
+      );
+    }, 1000 * 60);
+  }
+}
+
+module.exports = async function App() {
+  return router([
+    text('有 Switch 嗎？', HandleQuestion),
+    messenger.optin(HandleOptin),
+  ]);
+};
